ff6e9fdfe73ac6f891f433bd1700cf4b5a2806f8
Hugh McKenna <hfmckenna@outlook.com>
2025-02-22 14:05:11 +0000

    feat: Dial timeout

---
+++ dial_timeout.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net"
+	"syscall"
+	"time"
+)
+
+func DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
+	d := net.Dialer{
+		Control: func(_, addr string, _ syscall.RawConn) error {
+			return &net.DNSError{
+				Err:         "connection timeout",
+				Name:        addr,
+				Server:      "127.0.0.1",
+				IsTimeout:   true,
+				IsTemporary: true,
+			}
+		},
+		Timeout: timeout,
+	}
+	return d.Dial(network, address)
+}
---
+++ dial_timeout_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net"
+	"testing"
+	"time"
+)
+
+func TestDialTimeout(t *testing.T) {
+	c, err := DialTimeout("tcp", "10.0.0.1:http", 5*time.Second)
+	if err == nil {
+		c.Close()
+		t.Fatal(err)
+	}
+	nErr, ok := err.(net.Error)
+	if !ok {
+		t.Fatal(err)
+	}
+	if !nErr.Timeout() {
+		t.Fatal("not a timeout")
+	}
+}
